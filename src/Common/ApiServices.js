5d0dde90140a6235ba80fda50f680afae4fa9244
ashutoshmohanty143 <ashutoshmohanty143@gmail.com>
2022-10-25 21:11:19 +0530

    commit

--- src/Common/ApiServices.js
+++ src/Common/ApiServices.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-//const API_BASE_URL = "http://localhost:5000/api/curd";
-const API_BASE_URL = "http://thinkfast.in:5000/api/curd";
+const API_BASE_URL = "http://localhost:5000/api/curd";
+//const API_BASE_URL = "http://thinkfast.in:5000/api/curd";
 
 class ApiServices {
     
